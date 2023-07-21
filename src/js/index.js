d17a59f1974e4265ecf861ecbb0bc7618f8499f7
Mika <mikberza@mail.ru>
2023-07-21 14:34:33 +0300

    Кнопка, установите все флажки -разметка и стили для кнопки были завершены
    Button, check all checkboxes
    -markup and styles for the button have been completed

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,6 @@ import '../css/index.css';
 // импортируем изображения
 import '../assets/images/1.51bird.svg';
 import '../assets/images/VectorDown.svg';
+import '../assets/images/icon_checkbox_b.png';
+import '../assets/images/icon_checkbox_w.png';
+import '../assets/images/icon_checkbox_g.png';
