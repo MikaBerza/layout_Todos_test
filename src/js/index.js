b53c692bc565889d8a7db6da5f2610ae5020b4da
Mika <mikberza@mail.ru>
2023-07-22 06:01:21 +0300

    Изменение расположения кнопок -добавлена новая разметка кнопок (отметить, очистить) -добавлены стили для кнопок (отметить, очистить) -старая кнопка, отмечающая все флажки, скоро будет удалена
    Changing the location of buttons
    -added new button layout (mark, clear)
    -added styles for buttons (mark, clear)
    -the old button, check all the boxes, will be removed soon

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,11 @@ import '../css/index.css';
 // импортируем изображения
 import '../assets/images/1.51bird.svg';
 import '../assets/images/VectorDown.svg';
+
 import '../assets/images/icon_checkbox_b.png';
 import '../assets/images/icon_checkbox_w.png';
 import '../assets/images/icon_checkbox_g.png';
+
+import '../assets/images/icon_clear_b.png';
+import '../assets/images/icon_clear_w.png';
+import '../assets/images/icon_clear_g.png';
