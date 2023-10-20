fc9f6734ac654ef930ae87cc58ba8d02bbc07abd
Mika <mikberza@mail.ru>
2023-10-20 06:21:37 +0300

    Изменение названия кнопок Changing the name of the buttons

--- src/js/images.js
+++ src/js/images.js
@@ -9,11 +9,3 @@ import '../assets/images/fav_icons_bird/android-chrome-256x256.png';
 // импортируем изображения
 import '../assets/images/1.51bird.svg';
 import '../assets/images/VectorDown.svg';
-
-import '../assets/images/icon_checkbox_b.png';
-import '../assets/images/icon_checkbox_w.png';
-import '../assets/images/icon_checkbox_g.png';
-
-import '../assets/images/icon_clear_b.png';
-import '../assets/images/icon_clear_w.png';
-import '../assets/images/icon_clear_g.png';
